70e9447fadafc2b4071b14f0c1064d2907e0322d
mrmissx <hi@mrmiss.me>
2022-03-15 16:48:45 +0700

    Fix og meta image

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -3,26 +3,27 @@ import Head from "next/head"
 
 export default function Metadata() {
     const desc: string = "Hii... Welcome to my personal website."
-    const image: string = "http://mrmiss.me/profile.jpg"
+    const baseUrl: string = "https://mrmiss.my.id"
+    const imagePath: string = "/profile.jpg"
 
     return (
         <Head>
-            <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+            <meta name="image" content={ baseUrl + imagePath }/>
+            <link rel="icon" href={ baseUrl + "/favicon.ico"} />
             <meta name="description" content={ desc } />
-            <meta name="image" content={ image }/>
 
-            <meta property="og:title" content="Mr.Miss" />
             <meta property="og:type" content="article" />
-            <meta property="og:url" content="http://mrmiss.me" />
-            <meta property="og:image" content={ image } />
+            <meta property="og:title" content="Mr.Miss" />
+            <meta property="og:image" content={ baseUrl + imagePath } />
             <meta property="og:description" content={ desc } />
+            <meta property="og:url" content={ baseUrl } />
 
-            <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:site" content="@mrmissx"/>
-            <meta property="twitter:creator" content="@mrmissx" />
+            <meta property="twitter:card" content="summary"/>
             <meta property="twitter:title" content="MrMiss"/>
+            <meta property="twitter:site" content="@mrmissx"/>
+            <meta property="twitter:image" content={ baseUrl + imagePath } />
+            <meta property="twitter:creator" content="@mrmissx" />
             <meta property="twitter:description" content={ desc }/>
-            <meta property="twitter:image" content={ image } />
         </Head>
     )
 }
