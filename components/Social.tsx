f838c7f6edd6128df25ca7d1a5a5daccc22f78cf
mrmissx <hi@mrmiss.me>
2022-03-16 16:32:31 +0700

    componetnes: Social: use list data

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,52 +1,31 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 
 export default function Social() {
-    // const social = [
-    //     {link: "https://github.com/MrMissx", icon: ["fab", "github"]},
-    //     {link: "https://t.me/keselekpermen", icon: ["fab", "telegram"]},
-    //     {link: "https://instagram.com/gaungrb", icon: ["fab", "instagram"]},
-    //     {link: "https://twitter.com/mrmissx", icon: ["fab", "twitter"]},
-    //     {link: "https://www.linkedin.com/in/gaungr", icon: ["fab", "linkedin"]}
-    // ]
+    const social = [
+        {link: "https://github.com/MrMissx", icon: ["fab", "github"], hoverColor: "hover:text-gray-800"},
+        {link: "https://t.me/keselekpermen", icon: ["fab", "telegram"], hoverColor: "hover:text-blue-400"},
+        {link: "https://instagram.com/gaungrb", icon: ["fab", "instagram"], hoverColor: "hover:text-red-400"},
+        {link: "https://twitter.com/mrmissx", icon: ["fab", "twitter"] , hoverColor: "hover:text-blue-400"},
+        {link: "https://www.linkedin.com/in/gaungr", icon: ["fab", "linkedin"], hoverColor: "hover:text-blue-500"}
+    ]
 
     return (
-        <div className="flex justify-evenly mx-10 my-3 text-xl sm:text-3xl lg:mx-20">
-            <a href="https://github.com/MrMissx"
-                className="text-gray-400 hover:text-gray-800 mx-2"
-                role="button"
-                target="_blank"
-                rel="noopener noreferrer">
-                <FontAwesomeIcon icon={['fab', 'github']} />
-            </a>
-            <a href="https://t.me/keselekpermen"
-                className="text-gray-400 hover:text-blue-400 mx-2"
-                role="button"
-                target="_blank"
-                rel="noopener noreferrer">
-                <FontAwesomeIcon icon={['fab', 'telegram']} />
-            </a>
-            <a href="https://instagram.com/gaungrb"
-                className="text-gray-400 hover:text-red-400 mx-2"
-                role="button"
-                target="_blank"
-                rel="noopener noreferrer">
-                <FontAwesomeIcon icon={['fab', 'instagram']} />
-            </a>
-            <a href="https://twitter.com/mrmissx"
-                className="text-gray-400 hover:text-blue-400 mx-2"
-                role="button"
-                target="_blank"
-                rel="noopener noreferrer">
-                <FontAwesomeIcon icon={['fab', 'twitter']} />
-            </a>
-            <a href="https://www.linkedin.com/in/gaungr/"
-                className="text-gray-400 hover:text-blue-500 mx-2"
-                role="button"
-                target="_blank"
-                rel="noopener noreferrer">
-                <FontAwesomeIcon icon={['fab', 'linkedin']} />
-            </a>
+        <div className="flex justify-evenly mx-10 my-3 text-xt sm:text-3xl lg:mx-20">
+            {social.map((item, idx) => {
+                return (
+                    <a key={idx.toString()}
+                        href={item.link}
+                        className={"text-gray-400 mx-2" + item.hoverColor }
+                        role="button"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={item.icon as IconProp} className={ item.hoverColor } />
+                    </a>
+                )
+            })}
         </div>
     )
 }
