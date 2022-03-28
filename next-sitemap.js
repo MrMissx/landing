8242e42e9b286f65b02455ca5c92eafad9d6802d
mrmissx <hi@mrmiss.me>
2022-03-28 12:13:03 +0700

    Add sitemap generator

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+// Sitemap xml builder
+// Change this to your site url
+
+const siteUrl = "https://mrmiss.my.id";
+
+module.exports = {
+    siteUrl,
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+};
