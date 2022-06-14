3bd50e18673ed00ad0b7184a0bebb74257d4d89a
mrmissx <hi@mrmiss.me>
2022-06-14 20:59:23 +0700

    Fix sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,6 +7,7 @@ const siteUrl = "https://mrmiss.my.id";
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,
+    generateIndexSitemap: true,
     sitemapSize: 7000,
     exclude: [ "/admin/**" ]
 };
