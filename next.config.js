18d33c1502d26143375f2a5d9661af0fbcbaf6b9
mrmissx <hi@mrmiss.me>
2022-07-26 08:18:24 +0700

    Add page redirect
    All page redirection will use 307 status code (temporary redirect).

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
+const redirect = require("./redirects.json");
+
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['mrmiss.my.id', 'telegra.ph', 'i.ibb.co', 'firebasestorage.googleapis.com']
+  images: { 
+    domains: ["mrmiss.my.id", "firebasestorage.googleapis.com"]
+  },
+  redirects: async () => {
+    const res = [];
+    for (const path in redirect) {
+      res.push({ source: "/" + path, destination: redirect[path], permanent: false });
+    }
+    return res;
   }
 }
