9de16005bea34c069a27117a8dfbf381e4c62bbd
Gaung Ramadhan <hi@mrmiss.me>
2022-09-18 13:12:30 +0700

    Purge fontawesome icons and change to react-icons (#180)

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,14 +1,13 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FaGithub, FaTelegram, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa'
 
 
 export default function Social() {
     const social = [
-        {link: "https://github.com/MrMissx", icon: ["fab", "github"], hoverColor: "hover:text-gray-800 dark:hover:text-gray-200", label: "github"},
-        {link: "https://t.me/MrMissx", icon: ["fab", "telegram"], hoverColor: "hover:text-blue-400", label: "telegram"},
-        {link: "https://instagram.com/gaungrb", icon: ["fab", "instagram"], hoverColor: "hover:text-red-400", label: "instagram"},
-        {link: "https://twitter.com/mrmissx", icon: ["fab", "twitter"] , hoverColor: "hover:text-blue-400", label: "twitter"},
-        {link: "https://www.linkedin.com/in/gaungr", icon: ["fab", "linkedin"], hoverColor: "hover:text-blue-500", label: "linkedin"}
+        { link: "https://github.com/MrMissx", icon: <FaGithub />, hoverColor: "hover:text-gray-800 dark:hover:text-gray-200", label: "github" },
+        { link: "https://t.me/MrMissx", icon: <FaTelegram />, hoverColor: "hover:text-blue-500 dark:hover:text-blue-500", label: "telegram" },
+        { link: "https://instagram.com/gaungrb", icon: <FaInstagram />, hoverColor: "hover:text-red-400 dark:hover:text-red-400", label: "instagram" },
+        { link: "https://twitter.com/mrmissx", icon: <FaTwitter />, hoverColor: "hover:text-blue-400 dark:hover:text-blue-400", label: "twitter" },
+        { link: "https://www.linkedin.com/in/gaungr", icon: <FaLinkedin />, hoverColor: "hover:text-blue-500 dark:hover:text-blue-500", label: "linkedin" }
     ]
 
     return (
@@ -17,13 +16,13 @@ export default function Social() {
                 return (
                     <a key={idx.toString()}
                         href={item.link}
-                        className={"text-gray-400 dark:text-stone-600 mx-2" + item.hoverColor }
+                        className={"text-gray-400 dark:text-stone-600 mx-2 " + item.hoverColor}
                         role="button"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label={item.label}
                     >
-                        <FontAwesomeIcon icon={item.icon as IconProp} className={ item.hoverColor } />
+                        {item.icon}
                     </a>
                 )
             })}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,28 +4,14 @@ import { motion } from 'framer-motion'
 import { ThemeProvider } from 'next-themes'
 import NextNProgress from "nextjs-progressbar";
 
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { faAngleUp, faMoon } from '@fortawesome/free-solid-svg-icons'
-import {
-    faGithub,
-    faTwitter,
-    faLinkedin,
-    faInstagram,
-    faTelegram
-} from '@fortawesome/free-brands-svg-icons'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-
 import AdminContext from "../context/adminContext";
-
-config.autoAddCss = false
-library.add(faGithub, faTwitter, faLinkedin, faInstagram, faTelegram, faAngleUp, faMoon)
 
 
 function MyApp({ Component, pageProps, router }: AppProps) {
     return (
         <AdminContext>
             <ThemeProvider attribute="class">
-                <NextNProgress height={4} options={{ showSpinner: false }}/>
+                <NextNProgress height={4} options={{ showSpinner: false }} />
                 <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
                     pageInitial: {
                         opacity: 0,
@@ -34,7 +20,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
                         opacity: 1,
                     },
                 }}>
-                        <Component {...pageProps} />
+                    <Component {...pageProps} />
                 </motion.div>
             </ThemeProvider>
         </AdminContext>
